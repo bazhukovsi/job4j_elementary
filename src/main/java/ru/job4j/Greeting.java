45c2d58c7e044f70d54168a4a48bbd73f1cecb1a
Сергей <bazhukovsi@yandex.ru>
2021-11-19 19:11:27 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/Greeting.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+//         2. Внутри метода main выполните следующие действия:
+//         - создайте переменную с именем idea типа String и присвойте ей значение "I like Java!";
+//         - выведите на консоль эту переменную через команду System.out.println(idea);
+//         - присоедините к переменной idea строку "But I am a newbie.";
+//         - создайте переменную year с типом int и присвойте ей текущий год.
+//         - присоедините к переменной idea переменную year;
+//         - выведите на консоль эту переменную через команду System.out.println(idea);
+
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        String newIdea = idea + "But I am a newbie.";
+        int year = 2021;
+        String yearIdea = newIdea + year;
+        System.out.println(yearIdea);
+    }
+}
