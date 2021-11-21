eb0a1f9423e8ddc1e2251345b6b982281f78784b
Сергей <bazhukovsi@yandex.ru>
2021-11-21 09:50:45 +0300

    9.1. String [#237407]

--- src/main/java/ru/job4j/ArgMethod.java
+++ src/main/java/ru/job4j/ArgMethod.java
@@ -10,21 +10,13 @@ public class ArgMethod {
     public static void hello(String name, int age) {
         System.out.println("Hello, " + name + ", age = " + age);
     }
-    public static void hello(String name) {
-        System.out.println("Hello, " + name + ", age = 6");
-    }
-    public static void hello(int age) {
-        System.out.println("Hello, Job4j, age = " + age);
-    }
-    public static void hello(int age,String name) {
-        System.out.println("Hello, " + name + ", age = " + age);
-    }
+
     public static void main(String[] args) {
         String name = "Job4j";
         int age = 6;
-        ArgMethod.hello(name);
         ArgMethod.hello(name, age);
-        ArgMethod.hello(age);
-        ArgMethod.hello(age, name);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
+        ArgMethod.hello(name, age);
     }
 }
