802c86e4de66877a0f614f8ff10af52af1cdbedf
Сергей <bazhukovsi@yandex.ru>
2021-11-22 18:56:49 +0300

    4.4. Вложенный оператор if else. Запутанный код [#190 #231261]

---
+++ src/main/java/ru/job4j/condition/ArithmeticActions.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+
+public class ArithmeticActions {
+
+    public static String checkNumber(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "Hello, World!!!";
+        } else if (number % 3 == 0) {
+            return "Hello";
+        } else if (number % 5 == 0) {
+            return "World";
+        } else {
+            return "Operation not support";
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkNumber(15));
+
+    }
+}
+
