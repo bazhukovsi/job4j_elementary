78800c546434d8d4c267164c61dbc5d3a3215d3b
Сергей <bazhukovsi@yandex.ru>
2021-11-24 00:29:59 +0300

    Alphabet

---
+++ src/main/java/ru/job4j/arrayLists/Alphabet.java
@@ -0,0 +1,29 @@
+package ru.job4j.arrayLists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Alphabet {
+    public static String reformat(String s) {
+        StringBuilder sb = new StringBuilder();
+        List<String> list = new ArrayList<>();
+        String out;
+        String [] strings = s.split("");
+        for (int i = 0; i < strings.length; i++) {
+            list.add(strings[i]);
+        }
+        list.sort(Comparator.naturalOrder());
+        for (String value : list) {
+            sb.append(value);
+        }
+        out = sb.toString();
+        return out;
+    }
+
+    public static void main(String[] args) {
+        reformat("javascript");
+    }
+
+}
