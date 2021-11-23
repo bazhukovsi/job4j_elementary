d51a37cf3cbf13a8847d959ada1ceb79e662d104
Сергей <bazhukovsi@yandex.ru>
2021-11-23 17:38:54 +0300

    RemoveIf

---
+++ src/main/java/ru/job4j/arrayLists/RemoveIf.java
@@ -0,0 +1,30 @@
+package ru.job4j.arrayLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveIf {
+    public static List<String> sortList(List<String> list) {
+        list.removeIf(s -> s.length() < 5);
+        return list;
+    }
+
+    public static void main(String[] args) {
+        List <String> list = new ArrayList<>();
+        list.add("one");
+        list.add("two");
+        list.add("three");
+        list.add("four");
+        list.add("five");
+        list.add("six");
+        list.add("seven");
+        list.add("eight");
+        list.add("nine");
+        list.add("ten");
+        sortList(list);
+        for(String str : list){
+            System.out.println(str);
+        }
+    }
+
+}
