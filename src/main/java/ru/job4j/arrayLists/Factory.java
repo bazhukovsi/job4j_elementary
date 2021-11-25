2eeb80db0016da13bb08bfc630c25f98897b3440
Сергей <bazhukovsi@yandex.ru>
2021-11-25 06:31:21 +0300

    Исправления

--- src/main/java/ru/job4j/arrayLists/Factory.java
+++ src/main/java/ru/job4j/arrayLists/Factory.java
@@ -1,12 +1,10 @@
 package ru.job4j.arrayLists;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Factory {
     public static void main(String[] args) {
-        List<String> list = List.of("first","second","third","fourth","fifth");
+        List<String> list = List.of("first", "second", "third", "fourth", "fifth");
         for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
