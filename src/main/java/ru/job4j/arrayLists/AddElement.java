200a422c87e640f60635020b8a64ed42a8953dec
Сергей <bazhukovsi@yandex.ru>
2021-11-25 06:33:43 +0300

    Исправления

--- src/main/java/ru/job4j/arrayLists/AddElement.java
+++ src/main/java/ru/job4j/arrayLists/AddElement.java
@@ -7,14 +7,14 @@ public class AddElement {
     public static boolean addNewElement(List<String> list, String str) {
         List<String> check = new ArrayList<>(list);
         list.add(str);
-        if(list.size() != check.size()){
+        if (list.size() != check.size()) {
             return true;
         }
         return false;
     }
 
     public static void main(String[] args) {
-        List <String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         list.add("123");
         System.out.println(addNewElement(list, "String"));
     }
