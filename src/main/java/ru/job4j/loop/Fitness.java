904530dca3dcf2983cfc509a3342b30a6f3fc1b7
Сергей <bazhukovsi@yandex.ru>
2021-11-26 21:00:42 +0300

    5.4. Протеиновая диета [#156313 #232776]

--- src/main/java/ru/job4j/loop/Fitness.java
+++ src/main/java/ru/job4j/loop/Fitness.java
@@ -3,11 +3,9 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int count = 0;
-        int ivanProtein = 3;
-        int nikProtein = 2;
         while (ivan <= nik) {
-            ivan = ivanProtein * ivan;
-            nik = nikProtein * nik;
+            ivan *= 3;
+            nik *= 2;
             count++;
         }
         return count;
